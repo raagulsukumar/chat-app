b4c34e8d032888f08ee8dc6a1d576ff152920a46
Raagul Sukumar <raagul.sukumar@gmail.com>
2025-07-15 21:19:27 -0500

    begin  message model developement

--- model/message.py
+++ model/message.py
@@ -1,1 +1,22 @@
-message.py
+# Message main model.
+class message(object):
+ 
+  # Initializer.
+  def __init__(self):
+    pass
+ 
+  # Create message.
+  def create_message(self):
+    pass
+ 
+  # Send message.
+  def send_message(self):
+    pass
+ 
+  # Receive message.
+  def receive_message(self):
+    pass
+ 
+  # Delete message.
+  def delete_message(self):
+    pass
